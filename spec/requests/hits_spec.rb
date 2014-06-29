3f8b3f91596542c3464a6ff0bba9eea7a79f9419
Chris Oliver <excid3@gmail.com>
2014-06-29 12:41:47 -0500

    Update specs to expect empty days with empty array of results

--- spec/requests/hits_spec.rb
+++ spec/requests/hits_spec.rb
@@ -5,17 +5,29 @@ describe "Hits API" do
     before { Hit.where.destroy }
 
     it "responds with an empty json object" do
-      get "/top_urls"
+      get "/top_urls.json"
 
       expect(response).to be_success
-      expect(json).to eq({})
+      expect(json).to eq({
+        1.days.ago.to_date.to_s => [],
+        2.days.ago.to_date.to_s => [],
+        3.days.ago.to_date.to_s => [],
+        4.days.ago.to_date.to_s => [],
+        5.days.ago.to_date.to_s => [],
+      })
     end
 
     it "responds with an empty json object" do
-      get "/top_referrers"
+      get "/top_referrers.json"
 
       expect(response).to be_success
-      expect(json).to eq({})
+      expect(json).to eq({
+        1.days.ago.to_date.to_s => [],
+        2.days.ago.to_date.to_s => [],
+        3.days.ago.to_date.to_s => [],
+        4.days.ago.to_date.to_s => [],
+        5.days.ago.to_date.to_s => [],
+      })
     end
   end
 
@@ -29,18 +41,20 @@ describe "Hits API" do
     }
 
     it "responds with the number of page views per URL grouped by day for the past 5 days" do
-      get "/top_urls"
+      get "/top_urls.json"
 
       expect(response).to be_success
       expect(json).to eq({
         1.days.ago.to_date.to_s => [{"url" => @first.url, "visits" => 1}],
         2.days.ago.to_date.to_s => [{"url" => @second.url, "visits" => 1}],
         3.days.ago.to_date.to_s => [{"url" => @third.url, "visits" => 1}],
+        4.days.ago.to_date.to_s => [],
+        5.days.ago.to_date.to_s => [],
       })
     end
 
     it "responds with the top 5 referrers for the top 10 urls grouped by day for the past 5 days" do
-      get "/top_referrers"
+      get "/top_referrers.json"
 
       expect(response).to be_success
       expect(json).to eq({
@@ -59,6 +73,8 @@ describe "Hits API" do
             {"url" => @third.referrer, "visits" => 1}
           ]}
         ],
+        4.days.ago.to_date.to_s => [],
+        5.days.ago.to_date.to_s => [],
       })
     end
   end
